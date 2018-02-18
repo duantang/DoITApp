e8321eedc94ae42e7cc58f7fa69218cb2d1c3040
Kevin Schwarz <duantang@gmail.com>
2018-02-18 22:06:30 +0100

    Design fertig

---
+++ DoIT/Task.swift
@@ -0,0 +1,17 @@
+//
+//  Task.swift
+//  DoIT
+//
+//  Created by Kevin Schwarz on 18.02.18.
+//  Copyright © 2018 KSARC. All rights reserved.
+//
+
+import Foundation
+
+
+class Task {
+    
+    var name = ""
+    var important = false
+    
+}
--- DoIT/ViewController.swift
+++ DoIT/ViewController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableViewCT: UITableView!
+    
+    var tasks : [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tasks = makeTasks()
+        
+        tableViewCT.delegate = self
+        tableViewCT.dataSource = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row]
+        if task.important {
+            cell.textLabel?.text = "❗️WICHTIG❗️ \(task.name)"
+        
+        } else {
+            cell.textLabel?.text = task.name
+        }
+    
+          return cell
+    }
+      
+    func makeTasks() -> [Task] {
+        let task1 = Task()
+        task1.name = "Kim küssen"
+        task1.important = true
+        let task2 = Task()
+        task2.name = "Kim knuddeln"
+        task2.important = true
+        let task3 = Task()
+        task3.name = "Kim knutschen"
+        task3.important = true
+        return [task1, task2, task3]
+    }
+    @IBAction func addItem(_ sender: Any) {
+        performSegue(withIdentifier: "newTaskSegue", sender: nil)
+    }
+    
 }
 
